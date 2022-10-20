fe170da08523ce4494539354f364a70d07a328bd
Timotei Herembas <herembastimo@yahoo.com>
2022-10-20 17:00:05 +0300

    Add project files.

---
+++ Data/Herembas_Timotei_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Herembas_Timotei_Lab2.Models;
+
+namespace Herembas_Timotei_Lab2.Data
+{
+    public class Herembas_Timotei_Lab2Context : DbContext
+    {
+        public Herembas_Timotei_Lab2Context (DbContextOptions<Herembas_Timotei_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Herembas_Timotei_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Herembas_Timotei_Lab2.Models.Publisher> Publisher { get; set; }
+    }
+}
