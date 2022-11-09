161a9348bea719cf5ce3578422547c287498faa1
Timotei Herembas <herembastimo@yahoo.com>
2022-11-09 17:19:38 +0200

    Lab04

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,11 @@
+﻿using Herembas_Timotei_Lab2.Models;
+
+namespace Herembas_Timotei_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    
+    }
+
---
+++ Pages/Publishers/Author.cs
@@ -0,0 +1,22 @@
+﻿using Herembas_Timotei_Lab2.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Herembas_Timotei_Lab2.Pages.Publishers
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+        public ICollection<Book>? Books { get; set; }
+    }
+}
